50e94cc0304467a4bda16da55aa19f86de47503a
D-diary <dlrlsud@naver.com>
2022-01-12 03:04:22 +0900

    finish

--- app.py
+++ app.py
@@ -23,7 +23,18 @@ def get_matjip():
     # 2. 그걸 클라이언트에 돌려준다
     return jsonify({'result': 'success', 'matjip_list': matjip_list})
 
+@app.route('/like_matjip', methods=["POST"])
+def like_matjip():
+    title_receive = request.form["title_give"]
+    address_receive = request.form["address_give"]
+    action_receive = request.form["action_give"]
+    print(title_receive, address_receive, action_receive)
 
+    if action_receive == "like":
+        db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$set": {"liked": True}})
+    else:
+        db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$unset": {"liked": False}})
+    return jsonify({'result': 'success'})
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
